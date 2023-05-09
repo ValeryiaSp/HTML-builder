50812886be8695df774ba9201a1e28391232e999
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2023-05-09 14:32:13 +0300

    fix: add encoding and call od the function to the task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 const readingHandler = async () => {
   try {
     const fileToReadPath = path.join(__dirname, 'text.txt');
-    const stream = fs.createReadStream(fileToReadPath);
-    stream.on('data', (text) => process.stdout.write(text));
+    const stream = fs.createReadStream(fileToReadPath, { encoding: 'utf-8' });
+    stream.on('data', (chunk) => process.stdout.write(chunk));
+    stream.on('end', () => process.stdout.write('\n'));
   } catch (error) {
     throw Error(error);
   }
 };
+
+readingHandler();
